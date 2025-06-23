7c8987382558e1979153888dbcf6746917dffd7a
Farid Azhari <faridazhari07@gmail.com>
2025-06-24 05:36:50 +0700

    setup main jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {BrowserRouter, Route, Routes} from "react-router";
+import Layout from "./components/Layout";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+      <BrowserRouter>
+          <Routes>
+              <Route  element={<Layout/>}>
+                  <Route path="/register" element={<div>Register</div>}></Route>
+              </Route>
+              <Route path="/dashboard" ></Route>
+
+          </Routes>
+      </BrowserRouter>
   </StrictMode>,
 )
