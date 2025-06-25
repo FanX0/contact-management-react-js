b5a6c91576dc352287608e24be1dabfc9920d468
Farid Azhari <faridazhari07@gmail.com>
2025-06-26 03:28:48 +0700

    add dashboardlayoout, user login, user update

--- src/lib/api/UserApi.js
+++ src/lib/api/UserApi.js
@@ -5,6 +5,72 @@ export const userRegister = async ({username, password, name}) => {
             'Content-Type': 'application/json',
             'Accept': 'application/json',
         },
-        body: JSON.stringify({username, password, name})
+        body: JSON.stringify({
+            username,
+            password,
+            name
+        }),
+    })
+}
+
+export const userLogin = async ({username, password}) => {
+    return await fetch(`${import.meta.env.VITE_API_PATH}/users/login`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            username,
+            password
+        }),
+    })
+}
+
+export const userUpdateProfile = async (token, {name}) => {
+    return await fetch(`${import.meta.env.VITE_API_PATH}/users/current`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify({
+            name
+        }),
+    })
+}
+
+export const userUpdatePassword = async (token, {password}) => {
+    return await fetch(`${import.meta.env.VITE_API_PATH}/users/current`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify({
+            password
+        }),
+    })
+}
+
+export const userDetail = async (token) => {
+    return await fetch(`${import.meta.env.VITE_API_PATH}/users/current`, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': `Bearer ${token}`
+        }
+    })
+}
+
+export const userLogout = async (token) => {
+    return await fetch(`${import.meta.env.VITE_API_PATH}/users/logout`, {
+        method: 'DELETE',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': `Bearer ${token}`
+        }
     })
 }
