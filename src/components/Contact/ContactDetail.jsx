14700d5876d1c00a933c444b93d876a267567b31
Farid Azhari <faridazhari07@gmail.com>
2025-06-27 09:24:59 +0700

    add Address Ediit

--- src/components/Contact/ContactDetail.jsx
+++ src/components/Contact/ContactDetail.jsx
@@ -158,6 +158,12 @@ export default function ContactDetail() {
                                         <span>{address.postal_code}</span>
                                     </p>
                                 </div>
+                                <div className="flex justify-end space-x-3">
+                                    <Link to={`/dashboard/contacts/${id}/addresses/${address.id}/edit`}
+                                          className="px-4 py-2 bg-gradient text-white rounded-lg hover:opacity-90 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-gray-800 transition-all duration-200 font-medium shadow-md flex items-center">
+                                        <i className="fas fa-edit mr-2"></i> Edit
+                                    </Link>
+                                </div>
                             </div>
                         ))}
                     </div>
